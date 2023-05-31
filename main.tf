fa7dd4d2b8587eb698f7aff16ba96a7c590c66f9
watyanabe164 <50786237+watyanabe164@users.noreply.github.com>
2023-05-31 12:19:56 +0000

    新規作成

---
+++ main.tf
@@ -0,0 +1,74 @@
+# main.tf
+
+provider "azurerm" {
+  features {}
+}
+
+# Terraformの設定
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+  }
+}
+
+# リソースグループの作成
+resource "azurerm_resource_group" "example" {
+  name     = "example-resource-group"
+  location = "Japan East"
+}
+
+# 仮想ネットワークの作成
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# サブネットの作成
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+# 仮想マシン1の作成
+resource "azurerm_virtual_machine" "example1" {
+  name                  = "example-vm1"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example1.id]
+
+  vm_size              = "Standard_DS1_v2"
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "example-vm1"
+    admin_username = "adminuser"
+    admin_password = "watanabE164!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+}
